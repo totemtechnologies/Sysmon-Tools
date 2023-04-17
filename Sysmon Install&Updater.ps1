3d5e535f24e35c2bf07ed3258815c7f51ad2ba14
Tbone0825 <130683937+Tbone0825@users.noreply.github.com>
2023-04-17 12:09:30 -0600

    Updated Sysmon_setup.ps1 to V 1.1.0
    v 1.1 -  Created foundation for installer log, to be implemented at a later date  
        Improved installer flow 
        Added check for running as an admin
        Minor typo fixes
        Changed name to Sysmon_setup

--- Sysmon Install&Updater.ps1
+++ Sysmon Install&Updater.ps1
@@ -1,23 +1,74 @@
-﻿<#  
+<#  
 
 Sysmon Management
 
-V1.0.1
 
 This is a demo script inteded for use with sysmon to setup a new installation and if not present, install.  If installation is present
 pull the configuration version and do a check with GitHub.  
 
 https://github.com/MicrosoftDocs/sysinternals/blob/main/sysinternals/downloads/sysmon.md
+https://github.com/totemtechnologies/Sysmon-Tools
+
+Version 1.1.0
+
+v 1.1 -  Created foundation for installer log, to be implemented at a later date  
+    Improved installer flow 
+    Added check for running as an admin
+    Minor typo fixes
+    Changed name to Sysmon_setup
+
+v 1.0.1 - Minor corrections to initial release
 
 
 #>
 
+#Requires -RunAsAdministrator
+
+function Test-Administrator  
+{  
+    $user = [Security.Principal.WindowsIdentity]::GetCurrent();
+    (New-Object Security.Principal.WindowsPrincipal $user).IsInRole([Security.Principal.WindowsBuiltinRole]::Administrator)  
+}
+
+If (Test-Administrator) {
+    Write-Host "User is running as an admin"
+    } else {
+    Write-Host "User is not running as an admin, exiting"
+    Write-Host " "
+    Pause
+    Exit
+    }
+
+Set-ExecutionPolicy -ExecutionPolicy bypass
+
+##Getting started by making the proper log folders in \windows\logs
+
+## First make the log spot##
+$SysLog = 'Sysmon Logs'
+$Folder = 'C:\Windows\logs\sysmon logs'
+function Get-TimeStamp {return "[{0:MM/dd/yy} {0:HH:mm:ss}]" -f (Get-Date)}
+
+"Test to see if folder [$Folder] exists"
+if (Test-Path -Path $Folder) {
+    Write-host "Sysmon Log folder is present"
+    #Exit
+} else {
+    Write-host "Sysmon log folder is not present"
+    New-Item -ItemType Directory -path C:\Windows\Logs -name $syslog
+    Write-host "all done"
+    #Exit
+}
+
+Write-Output "$(Get-TimeStamp) starting syslog log installer/update file" | Out-File -filepath $folder\syslog_log.txt 
+
+
 #download from sysinternals github
-        Invoke-WebRequest 'https://download.sysinternals.com/files/Sysmon.zip' -outfile "C:\Windows\Temp\sysmon.zip"
+        Invoke-WebRequest 'https://download.sysinternals.com/files/Sysmon.zip' -outfile "C:\Windows\Temp\sysmon.zip" 
 #blowup the zip and drop in temp
-        Expand-archive -literalpath C:\Windows\Temp\sysmon.zip -DestinationPath C:\Windows\Temp 
+        Expand-archive -literalpath C:\Windows\Temp\sysmon.zip -DestinationPath C:\Windows\Temp | Write-Output "$(Get-TimeStamp) Unpacking zip file to temp directory"
 #Configfile for HBA
-        Invoke-WebRequest 'https://raw.githubusercontent.com/totemtechnologies/Sysmon-Tools/main/sysmonconfig-export.xml' -outfile "C:\Windows\sysmonconfig-export.xml"
+        Invoke-WebRequest 'https://raw.githubusercontent.com/totemtechnologies/Sysmon-Tools/main/sysmonconfig-export.xml' -outfile "C:\Windows\sysmonconfig-export.xml" 
+
 
 ## Variables in call ##
 
@@ -30,33 +81,40 @@ Function stop-Sy64service {stop-service -name Sysmon64}
 $temppath = "c:\windows\temp"
 $conf = "c:\windows\sysmonconfig-export.xml"
 
-CLS
+
+$logpath = 'C:\windows\logs\sysmon logs'
+$start = (Get-Date).addMinutes(-1440)
+$Events = 22,3,1
+$logfilehost = hostname
+
+#CLS
 
 ##Heres the run##
 
 if (get-service $service) {
-    CLS
-    write-host "Sysmon is already installed, checking for updates"
+
+#CLS
+    write-host "Sysmon is already installed, checking for updates" 
     Function stop-Sy64service {stop-service -name Sysmon64}
 
         If ($CurVer -lt $newver){
         
-        Write-Host "current sysmon is out of date"
+        Write-Host "current sysmon is out of date" 
         Start-Process -wait -FilePath "$temppath\sysmon64.exe" -ArgumentList "/u"
 
         Start-Process -wait -FilePath "$temppath\sysmon64.exe" -ArgumentList "/i $conf" 
         
         } else {
         
-        write-host "Current version is newer"
+        write-host "Current version is newer" 
         Start-Process -wait -FilePath "$temppath\sysmon64.exe" -ArgumentList "/c $conf"
-        write-host "We updated your configuration"
+        write-host "We updated your configuration" 
         
         }
     } else { 
     
-    CLS
-    Write-host "sysmon is not installed, installing"
+#CLS
+    Write-host "sysmon is not installed, installing" 
         Start-Process -wait -FilePath "$temppath\sysmon64.exe" -ArgumentList "/i $conf"
 
     }
@@ -65,4 +123,15 @@ if (get-service $service) {
 Get-ChildItem -Path "C:\Windows\Temp" *sysmon* -Recurse | Remove-Item -Force -Recurse
 Get-ChildItem -Path "C:\Windows\Temp" *eula* -Recurse | Remove-Item -Force -Recurse
 
+#stop-Sy64service
 
+#LinusTechTips
+#Modify SecPol to support 
+secedit /export /cfg c:\secpol.cfg
+ (gc C:\secpol.cfg).replace("AuditProcessTracking = 0", "AuditProcessTracking = 3") | Out-File C:\secpol.cfg
+ secedit /configure /db c:\windows\security\local.sdb /cfg c:\secpol.cfg /areas SECURITYPOLICY
+ rm -force c:\secpol.cfg -confirm:$false
+
+#start-Sy64service 
+
+Set-ExecutionPolicy -ExecutionPolicy default
